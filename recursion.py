84f5dfad282bfc18902bc20eb24a6b80f7487a53
Naveen M <naveen.miriyalu@gmail.com>
2020-06-24 10:46:57 +0530

    Recursion example -factorial

---
+++ recursion.py
@@ -0,0 +1,21 @@
+#Recursion
+
+#Basically calling the same function 
+#Writing code very simple
+
+#Factorial of  a number 
+# 3! = 3 * 2* 1
+
+def factorial(n):
+	if n == 0:
+		return 1
+	return n *factorial(n-1)
+
+def frac_iterative(n)
+	result = 1
+	while n != 0
+		result = result * n
+		n = n-1
+
+print(factorial(100))
+
